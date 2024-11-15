4df147608579cbe878530990e0a27f3eb387765c
notSaqlain <iamnotsaqlain@gmail.com>
2024-11-15 09:17:20 +0100

    feat: Add setters to Barca class and update Main class

--- Barca.java
+++ Barca.java
@@ -32,4 +32,32 @@ public class Barca {
     public boolean getTipologia(){
         return tipologia;
     }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setNazionalita(String nazionalita){
+        this.nazionalita = nazionalita;
+    }
+
+    public void setLunghezza(int lunghezza){
+        this.lunghezza = lunghezza;
+    }
+
+    public void setStazza(int stazza){
+        this.stazza = stazza;
+    }
+
+    public void setTipologia(boolean tipologia){
+        this.tipologia = tipologia;
+    }
+
+    public String toString(){
+        if (tipologia){
+            return "Nome: " + nome + " | Nazionalita: " + nazionalita + " | Lunghezza: " + lunghezza + " | Stazza: " + stazza + " | Tipologia: Motore";
+        } else {
+            return "Nome: " + nome + " | Nazionalita: " + nazionalita + " | Lunghezza: " + lunghezza + " | Stazza: " + stazza + " | Tipologia: Vela";
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,20 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        ArrayList<Barca> barche = new ArrayList<Barca>();
+        
+        Barca b1 = new Barca("Barca1", "Italia", 10, 100, true);
+        Barca b2 = new Barca("Barca2", "Spagna", 20, 200, false);
+        Barca b3 = new Barca("Barca3", "Francia", 30, 300, true);
+        
+        barche.add(b1);
+        barche.add(b2);
+        barche.add(b3);
+        
+        for(Barca b : barche){
+            System.out.println(b);
+        }
+
     }
 }
