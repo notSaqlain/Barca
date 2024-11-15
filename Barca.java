cce0e1b71a16d7504bb6160021b1508386ed4282
notSaqlain <iamnotsaqlain@gmail.com>
2024-11-15 09:11:55 +0100

    refactor: Add getters and constructor to Barca class

--- Barca.java
+++ Barca.java
@@ -1,3 +1,35 @@
 public class Barca {
-    
+    private String nome;
+    private String nazionalita;
+    private int lunghezza;
+    private int stazza;
+    private boolean tipologia;
+
+    public Barca(String nome, String nazionalita, int lunghezza, int stazza, boolean tipologia) {
+        this.nome = nome;
+        this.nazionalita = nazionalita;
+        this.lunghezza = lunghezza;
+        this.stazza = stazza;
+        this.tipologia = tipologia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNazionalita(){
+        return nazionalita;
+    }
+
+    public int getLunghezza(){
+        return lunghezza;
+    }
+
+    public int getStazza(){
+        return stazza;
+    }
+
+    public boolean getTipologia(){
+        return tipologia;
+    }
 }
